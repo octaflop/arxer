3dadd6e560869fce686ca9321d05a6a26dc12799
Faris Chebib <faris@legionofevil.org>
2011-03-15 15:50:47 -0700

    Deleted old db. added some clean urls for the front-forms

--- arxer/apps/verbena/urls/organization.py
+++ arxer/apps/verbena/urls/organization.py
@@ -10,16 +10,18 @@ org_list = {
 
 org_add = {
     "model" : Organization,
+    "login_required" : True,
 }
 
 org_edit = {
     "form_class" : OrganizationForm,
+    "login_required" : True,
 }
 
 urlpatterns = patterns("",
     url(r"^$", object_list, org_list, name="org_home"),
     url(r"^(?P<slug>[-\w]+)$", object_detail, org_list, name="org_view"),
-    url(r"^add$", create_object, org_add, name="org_add"),
+    url(r"^add/$", create_object, org_add, name="org_add"),
     url(r"^(?P<slug>[-\w]+)/edit$", update_object, org_edit, name="org_edit"),
 )
 
