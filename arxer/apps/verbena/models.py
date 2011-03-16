34a07ff2d3f20c258b1cf6e2e6a2861d45dd6d42
Faris Chebib <faris@legionofevil.org>
2011-03-15 18:05:22 -0700

    added fancy details for client's spec

--- arxer/apps/verbena/models.py
+++ arxer/apps/verbena/models.py
@@ -6,6 +6,16 @@ from django.utils.translation import ugettext_lazy as _
 from pinax.apps.profiles.models import Profile
 from pinax.apps.tribes.models import Tribe
 
+# Project types
+PROJECT_TYPE = (
+    (_("CO"), _("Computer Related")),
+    (_("CR"), _("Creative")),
+    (_("FR"), _("Formal Research")),
+    (_("OD"), _("Organizational Development")),
+    (_("SU"), _("Survey")),
+    (_("OT"), _("Other")),
+)
+
 # Possible Project statuses
 PROJECT_APPROVAL_STATUS = (
     (_("PR"), _("Proposed")),
@@ -34,11 +44,37 @@ class Organization(models.Model):
     """ Organizations are entities applying for projects or grants.
     Organizations share a single login """
     name = models.CharField(_("Name"), max_length=80)
-    slug = models.SlugField(_("Slug"), max_length=80)
+    slug = models.SlugField(_("URL-friendly name"), max_length=80)
+    community = models.CharField(
+            _("What community do you represent or work with?"),
+            max_length=180,
+            blank=True)
+    mandate = models.CharField(
+            _("What is your organization's goal or mandate?"),
+            max_length=180,
+            blank=True)
+    service = models.TextField(
+            _("How does your organization goal or mandate fit in with our\
+                desire to support community organizations?"),
+            blank=True)
+    funding = models.TextField(
+            _("What are your organization's principal sources of funding?"),
+            blank=True)
+    annual_budget = models.CharField(
+            _("What is your organization's annual budget?"),
+            max_length=25,
+            blank=True)
+    nonprofit_status = models.BooleanField(
+            _("Are you a registered non-profit?"),
+            default=False)
+
     about = models.TextField(_("About"))
     location = models.ForeignKey("Location")
     website = models.URLField(_("website"), blank=True, null=True)
-    workshops = models.ForeignKey("Workshop", blank=True, null=True)
+    workshops = models.ManyToManyField("Workshop",
+            related_name = "organization-workshops",
+            verbose_name = "organization's workshops",
+            blank=True, null=True)
 
     def __unicode__(self):
         return self.name
@@ -109,8 +145,8 @@ class Event(models.Model):
     """
     title = models.CharField(_("Event title"), max_length=100)
     slug = models.SlugField(_("Event slug"))
-    start_date = models.DateTimeField(_("Workshop start date & time"))
-    end_date = models.DateTimeField(_("Workshop end date & time"))
+    start_date = models.DateTimeField(_("Event start date & time"))
+    end_date = models.DateTimeField(_("Event end date & time"))
     location = models.ForeignKey(Location)
 
     def __unicode__(self):
@@ -125,10 +161,10 @@ class Workshop(Event):
     A workshop is an event with members
     """
     room = models.CharField(_("Room"), max_length=80, blank=True)
-    members = models.ManyToManyField(User,
+    members = models.ManyToManyField(Profile,
             related_name = "workshops",
             verbose_name = "members",
-            )
+        )
 
 class Gathering(Event):
     """
@@ -142,19 +178,46 @@ class VolunteerOpportunity(Event):
     """
     organization = models.ForeignKey(Organization)
     volunteers = models.ManyToManyField(User,
-        related_name = "volunteer opportunities",
-        verbose_name = "volunteers",
-    )
+            related_name = "volunteer opportunities",
+            verbose_name = "volunteers",
+        )
     class Meta:
         verbose_name = _("volunteer opportunity")
         verbose_name_plural = _("volunteer opportunities")
 
 # Project-Based Models
 class Project(Tribe):
-    title = models.CharField(_("Project title"), max_length=80)
-    approval_status = models.CharField(_("approval status"),\
+    title = models.CharField(
+            _("Project title (be clear and short)"),
+            max_length=80)
+    app_date = models.DateField(_("Project start date"))
+    research_question = models.TextField(
+            _("What is the central research question you want answered?"),
+            blank=True
+            )
+    project_type = models.CharField(
+            _("What type of project is this?"),
+            max_length=2, choices=PROJECT_TYPE, blank=False
+            )
+    magnitude = models.TextField(
+            _("Describe the size of the project in quantifiable terms\
+                (e.g. word/page count, duration of radio, number of hours)."),
+            )
+    project_description = models.TextField(
+            _("Please describe your project here")
+            )
+    issues = models.TextField(
+            _("What social/environmental issues are addressed by this project?")
+            )
+    results_plan = models.TextField(
+            _("How do you plan on using the results of this project?")
+            )
+    goal = models.TextField(
+            _("What larger goal is served by undertaking this project?")
+            )
+    approval_status = models.CharField(_("approval status"),
             max_length=2, choices=PROJECT_APPROVAL_STATUS, blank=False)
-    progress_status = models.CharField(_("progress status"),\
+    progress_status = models.CharField(_("progress status"),
             max_length=2, choices=PROJECT_PROGRESS_STATUS, blank=False)
 
     def __unicode__(self):
