358048850d7fe1ec11ab77776cbc5c3ac7f5482d
Faris Chebib <faris@pineal.(none)>
2011-05-12 11:57:29 -0700

    some major deployment cleanups

--- lib/python2.6/site-packages/attachments/admin.py
+++
@@ -1,6 +0,0 @@
-from attachments.models import Attachment
-from django.contrib.contenttypes import generic
-
-class AttachmentInlines(generic.GenericStackedInline):
-    model = Attachment
-    extra = 1
--- lib/python2.6/site-packages/idios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns("",
-    url(r"^$", "idios.views.profiles", name="profile_list"),
-    url(r"^profile/(?P<username>[\w\._-]+)/$", "idios.views.profile", name="profile_detail"),
-    url(r"^edit/$", "idios.views.profile_edit", name="profile_edit"),
-)
