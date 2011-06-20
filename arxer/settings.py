a0b0c9206bce01dd5a76b0eb9dcb0c3c2d39c8c2
Faris Chebib <faris@legionofevil.org>
2011-06-20 11:12:28 -0700

    hopefully, this bootstrap works...

--- arxer/settings.py
+++ arxer/settings.py
@@ -74,7 +74,6 @@ STATICFILES_DIRS = [
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
-#ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = "y2pvlbgq1wc@7v2wd8m2j$)ao*^eh&nolb1u9b$y96vkcj5g^)"
@@ -142,7 +141,6 @@ COMBINED_INBOX_COUNT_SOURCES = [
 
 INSTALLED_APPS = [
     #admin tools
-    "grappelli",
     "filebrowser",
     "admin_tools",
     "admin_tools.theming",
@@ -329,10 +327,8 @@ TINYMCE_DEFAULT_CONFIG = {
     }
 
 
-#URL_FILEBROWSER_MEDIA = os.path.join(STATIC_URL, 'filebrowser')
-#PATH_FILEBROWSER_MEDIA = os.path.join(STATIC_ROOT, 'filebrowser')
-#FILEBROWSER_DIRECTORY = ''
-DIRECTORY = 'uploads'
+#URL_FILEBROWSER_MEDIA = os.path.join('/site_media/static/filebrowser/')
+#PATH_FILEBROWSER_MEDIA = os.path.join(STATIC_ROOT, 'filebrowser/')
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,5 @@
+import os, sys, parser
+import subprocess, shutil
 if "VIRTUAL_ENV" not in os.environ:
     sys.stderr.write("$VIRTUAL_ENV not found.\n\n")
     parser.print_usage()
@@ -5,6 +7,28 @@ if "VIRTUAL_ENV" not in os.environ:
 virtualenv = os.environ["VIRTUAL_ENV"]
 file_path = os.path.dirname(__file__)
 subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
-                 os.path.join(file_path, "arxer/project.txt")])
+                 os.path.join(file_path, "arxer/requirements/project.txt")])
 subprocess.call(["pip", "install", "-E", virtualenv, "--requirement",
                  os.path.join(file_path, "requirements.txt")])
+FB_DOWNLOAD = "https://github.com/wardi/django-filebrowser-no-grappelli.git"
+subprocess.call(["git", "clone", FB_DOWNLOAD, os.path.join(file_path,\
+    "django-filebrowser")])
+# build the source
+subprocess.call(["python", os.path.join(file_path, "django-filebrowser",
+    "setup.py"), "build"])
+# makeshift install if needed
+if not os.path.isdir(os.path.join(file_path, "lib", "python2.6", "site-packages",
+    "filebrowser")):
+    shutil.copytree(os.path.join(file_path, "django-filebrowser", "filebrowser"),
+        os.path.join(file_path, "lib", "python2.6", "site-packages",
+        "filebrowser"))
+
+# add custom settings file for pinax
+if os.path.isfile(os.path.join(file_path, "lib", "python2.6",
+    "site-packages", "filebrowser", "settings.py")):
+    shutil.move(os.path.join(file_path, "lib", "python2.6", "site-packages",
+    "filebrowser", "settings.py"), os.path.join(file_path, "lib", "python2.6",
+    "site-packages", "filebrowser", "settings.py.back"))
+shutil.copyfile(os.path.join(file_path, "arxer", "requirements",
+    "settings.py"), os.path.join(file_path, "lib", "python2.6",
+    "site-packages", "filebrowser", "settings.py"))
